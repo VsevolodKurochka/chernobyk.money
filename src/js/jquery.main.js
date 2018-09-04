7c0465cb8cbb64d38b0aa1b25f090eee2b0c506d
VsevolodKurochka <dramdoobas123456@gmail.com>
2018-09-04 21:22:24 +0300

    Add carousel for reviews

--- src/js/jquery.main.js
+++ src/js/jquery.main.js
@@ -12,39 +12,11 @@ $(document).ready(function(){
 	});
 
 	// Collapse
-
-		$(".collapse__group.active").find(".collapse__group-body").slideDown();
-
-		$('.collapse').on('click', '.collapse__group-header', function(){
-			var collapseInner = $(this).parents('.collapse').find('.collapse__group');
-
-			$(this)
-				.parent()
-				.toggleClass('active');
-
-			$(this)
-				.next()
-				.slideToggle('slow');
-
-			collapseInner
-				.not($(this).parent())
-				.removeClass('active');
-
-			collapseInner
-				.children('.collapse__group-body')
-				.not($(this).next())
-				.slideUp("slow");
-
-		});
-	// Tabs
-		$('[data-action="tab"]').click(function(){			
-			// Tab links toggle class
-				$(this).closest(".tabs__list").children("li").removeClass('active');
-				$(this).parent().addClass('active');
-			// Show tab content
-				var tabTarget = $(this).attr('data-target');
-				$(tabTarget).fadeIn(0);
-				$(".tabs__content > div").not($(tabTarget)).fadeOut(0);
-		});
-	// Develope
-});	
+	$('#reviews-carousel').owlCarousel({
+		loop: true,
+		nav: true,
+		items: 1,
+		dots: false,
+		navContainerClass: 'reviews__carousel-nav'
+	});
+});
